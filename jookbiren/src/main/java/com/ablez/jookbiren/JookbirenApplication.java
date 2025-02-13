ed42cb99b0a280147394deb37e657152d2ed04d3
otaeho <dhxogh123123@naver.com>
2025-02-13 22:17:19 +0900

    first commit

---
+++ jookbiren/src/main/java/com/ablez/jookbiren/JookbirenApplication.java
@@ -0,0 +1,13 @@
+package com.ablez.jookbiren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JookbirenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JookbirenApplication.class, args);
+	}
+
+}
