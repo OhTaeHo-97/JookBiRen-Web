9e4dfbff5872498f428d1989f146f13e5f2195e4
otaeho <dhxogh123123@naver.com>
2025-02-19 00:16:23 +0900

    fix(Constant): 에피소드3 별표 문제 개수 수정

--- jookbiren/src/main/java/com/ablez/jookbiren/episode3/utils/JookbirenConstant.java
+++ jookbiren/src/main/java/com/ablez/jookbiren/episode3/utils/JookbirenConstant.java
@@ -1,5 +1,5 @@
 package com.ablez.jookbiren.episode3.utils;
 
 public class JookbirenConstant {
-    public static final int STAR_QUIZ_COUNT = 4;
+    public static final int STAR_QUIZ_COUNT = 5;
 }
