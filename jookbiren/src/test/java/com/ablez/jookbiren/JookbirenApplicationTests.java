ac001e10ea142adfbf2f34a32012d023f33ac40c
otaeho <dhxogh123123@naver.com>
2025-02-23 23:21:01 +0900

    fix: 테스트 주석화

--- jookbiren/src/test/java/com/ablez/jookbiren/JookbirenApplicationTests.java
+++ jookbiren/src/test/java/com/ablez/jookbiren/JookbirenApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ablez.jookbiren;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JookbirenApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ablez.jookbiren;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JookbirenApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
