73f32be4c04a2f4070e71cbbcbd8885777824a2b
otaeho <dhxogh123123@naver.com>
2025-03-01 16:37:00 +0900

    fix(Answer): 에피소드3 탐정 점수 기준 수정, 정답 가져오는 로직 수정

--- jookbiren/src/main/java/com/ablez/jookbiren/episode3/answer/service/Ep3AnswerService.java
+++ jookbiren/src/main/java/com/ablez/jookbiren/episode3/answer/service/Ep3AnswerService.java
@@ -48,29 +48,40 @@ public class Ep3AnswerService {
         AnswerEp03 answer = answerRepository.findByQuiz(quizInfo.getPlaceCode(), quizInfo.getQuizNumber())
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUIZ_NOT_FOUND));
         if (quizInfo.getPlaceCode() == 0) {
-            Quiz0Ep03 quiz = quizInfoService.findByQuizNumberAndUser0(quizInfo.getQuizNumber(), user)
-                    .orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUIZ_HISTORY_NOT_FOUND));
+            Quiz0Ep03 quiz = quizInfoService.findByQuizNumberAndUser0(quizInfo.getQuizNumber(), user).orElse(null);
+            if (quiz == null) {
+                quiz = quizInfoService.insertQuiz0(quizInfo.getQuizNumber(), user);
+            }
+
             if (quiz.getGetAnswerTime() == null) {
                 quiz.setGetAnswerTime(LocalDateTime.now());
                 user.setAnswerCount(user.getAnswerCount() + 1);
             }
         } else if (quizInfo.getPlaceCode() == 1) {
-            Quiz1Ep03 quiz = quizInfoService.findByQuizNumberAndUser1(quizInfo.getQuizNumber(), user)
-                    .orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUIZ_HISTORY_NOT_FOUND));
+            Quiz1Ep03 quiz = quizInfoService.findByQuizNumberAndUser1(quizInfo.getQuizNumber(), user).orElse(null);
+            if (quiz == null) {
+                quiz = quizInfoService.insertQuiz1(quizInfo.getQuizNumber(), user);
+            }
             if (quiz.getGetAnswerTime() == null) {
                 quiz.setGetAnswerTime(LocalDateTime.now());
                 user.setAnswerCount(user.getAnswerCount() + 1);
             }
         } else if (quizInfo.getPlaceCode() == 2) {
-            Quiz2Ep03 quiz = quizInfoService.findByQuizNumberAndUser2(quizInfo.getQuizNumber(), user)
-                    .orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUIZ_HISTORY_NOT_FOUND));
+            Quiz2Ep03 quiz = quizInfoService.findByQuizNumberAndUser2(quizInfo.getQuizNumber(), user).orElse(null);
+            if (quiz == null) {
+                quiz = quizInfoService.insertQuiz2(quizInfo.getQuizNumber(), user);
+            }
+
             if (quiz.getGetAnswerTime() == null) {
                 quiz.setGetAnswerTime(LocalDateTime.now());
                 user.setAnswerCount(user.getAnswerCount() + 1);
             }
         } else if (quizInfo.getPlaceCode() == 3) {
-            Quiz3Ep03 quiz = quizInfoService.findByQuizNumberAndUser3(quizInfo.getQuizNumber(), user)
-                    .orElseThrow(() -> new BusinessLogicException(ExceptionCode.QUIZ_HISTORY_NOT_FOUND));
+            Quiz3Ep03 quiz = quizInfoService.findByQuizNumberAndUser3(quizInfo.getQuizNumber(), user).orElse(null);
+            if (quiz == null) {
+                quiz = quizInfoService.insertQuiz3(quizInfo.getQuizNumber(), user);
+            }
+
             if (quiz.getGetAnswerTime() == null) {
                 quiz.setGetAnswerTime(LocalDateTime.now());
                 user.setAnswerCount(user.getAnswerCount() + 1);
@@ -158,13 +169,13 @@ public class Ep3AnswerService {
 
     private int calculateScore(UserEp03 user) {
         int score = 0;
-        if (user.getCriminal() == 2) {
+        if (user.getCriminal() == 1) {
             score += 5;
         } else {
             score += 1;
         }
 
-        if (user.getAnswerCount() == 0) {
+        if (user.getAnswerCount() <= 1) {
             score += 5;
         } else if (user.getAnswerCount() <= 3) {
             score += 4;
@@ -174,11 +185,11 @@ public class Ep3AnswerService {
             score += 1;
         }
 
-        if (user.getSolvedQuizCount() == 20) {
+        if (user.getSolvedQuizCount() == 16) {
             score += 5;
-        } else if (user.getSolvedQuizCount() >= 17) {
+        } else if (user.getSolvedQuizCount() >= 13) {
             score += 3;
-        } else if (user.getSolvedQuizCount() >= 14) {
+        } else if (user.getSolvedQuizCount() >= 10) {
             score += 2;
         } else {
             score += 1;
